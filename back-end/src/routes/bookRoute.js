c238d6fbf42ab7c4bb84fd6c90bec7189bbae60d
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-06-25 20:42:24 +0530

    qury for update book data

--- back-end/src/routes/bookRoute.js
+++ back-end/src/routes/bookRoute.js
@@ -52,3 +52,31 @@ router.post("/", (req, res) => {
     }
   );
 });
+
+// UPDATE
+router.put("/", (req, res) => {
+    const book_ID_data = req.body.book_ID;
+    const book_name_data = req.body.book_name;
+    const book_author_data = req.body.book_author;
+    const book_price_data = req.body.book_price;
+    var query =
+      "UPDATE book SET book_name=?,book_author=?,book_price=? WHERE book_ID=?";
+    connection.query(
+      query,
+      [book_name_data, book_author_data, book_price_data, book_ID_data],
+      (err, rows) => {
+        if (err) {
+          throw err;
+        } else {
+          if (rows.affectedRows > 0) {
+            res.send({ message: "book updated" });
+          } else {
+            res.send({ message: "book not found" });
+          }
+        }
+      }
+    );
+    console.log(req.body);
+  });
+
+  
