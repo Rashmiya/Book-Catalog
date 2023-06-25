02c6858ab58be8b2c0f5697f99fc2413132650d9
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-06-25 18:59:56 +0530

    content updated

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const book = require("./src/routes/bookRoute");
+
+app.use(express.json());
+app.use("/book",book);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
